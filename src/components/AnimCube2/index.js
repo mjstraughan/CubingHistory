e3160fbfd0b3ce517998ea1d9ed478a1ab04e7fc
Michael James Straughan <97840409+mjstraughan@users.noreply.github.com>
2024-03-31 19:49:36 -0400

    Create index.js

---
+++ src/components/AnimCube2/index.js
@@ -0,0 +1,27 @@
+import { useEffect, useId, useRef } from "react";
+import AnimCube2 from "./AnimCube2";
+
+/**
+ * Embeds an AnimCube visualizer as a React component.
+ * Documentation for parameters: https://animcubejs.cubing.net/animcubejs.html#usage
+ * @author err0rcuber
+ * @example
+ * <AnimCube2 params="move=RUR'URU2R'U2&initrevmove=#" width="400px" height="300px"/>
+ */
+export default function AnimCube2({
+  params,
+  width = "200px",
+  height = "219px",
+}) {
+  const id = "animcube_" + useId();
+  const divRef = useRef();
+  useEffect(() => {
+    AnimCube2(`id=${id}&${params}`);
+    const canvas = divRef.current?.childNodes[0];
+    if (canvas) {
+      canvas.style.maxWidth = "100%";
+    }
+  }, [params]);
+
+  return <div ref={divRef} style={{ width, height, maxWidth: "100%" }} id={id} />;
+}
