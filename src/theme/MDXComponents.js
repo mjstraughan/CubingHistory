e11d9b1c36dd14aefedd589ecc341540fdfc1d30
straughan <athefre@gmail.com>
2025-05-24 14:16:52 -0400

    Add method timeline

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,8 +2,8 @@ import React from "react";
 // Importing the original mapper + our components according to the Docusaurus doc
 import MDXComponents from "@theme-original/MDXComponents";
 
-import Timeline from "@site/src/components/Timeline";
-import TimelineItem from "@site/src/components/Timeline/TimelineItem";
+import Timeline from "@site/src/components/TimeLine";
+import TimelineItem from "@site/src/components/Timeline/TimeLineItem";
 
 export default {
   // Reusing the default mapping
